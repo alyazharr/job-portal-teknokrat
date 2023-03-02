0dcc056fc5a2a5b4a116e009cbb18e30f451d2e1
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2023-03-02 19:54:20 +0700

    fix: adding static_root variable on settings.py + fix code smells

--- jobseeker/settings.py
+++ jobseeker/settings.py
@@ -111,6 +111,13 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
