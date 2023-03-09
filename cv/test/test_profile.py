f7be717263e1cd77cc35626ea449d5539540d455
Veloraine <bivanderr@gmail.com>
2023-03-09 10:52:12 +0700

    [RED] added more tests

--- cv/test/test_profile.py
+++ cv/test/test_profile.py
@@ -1,12 +1,15 @@
 from django.test import TestCase, Client
 from jobseeker.models import CV, Users
-from django.urls import reverse, resolve
-from ..views import *
+from django.urls import reverse
+from ..views import profile
 
 
 class ProfileTestCase(TestCase):
+    
+    def setUp(self):
+        global CV_PROFILE 
+        CV_PROFILE = "cv:profile"
 
-    def test_profile_if_user_is_logged_in(self):
         created_user = Users.objects.create(
             username="username",
             password="password",
@@ -14,7 +17,7 @@ class ProfileTestCase(TestCase):
             prodi_id=1,
             role_id=1
         )
-        created_cv = CV.objects.create(
+        CV.objects.create(
             user=created_user,
             profile="profile",
             posisi = "manager",
@@ -30,55 +33,34 @@ class ProfileTestCase(TestCase):
             prestasi = "Juara 1 lomba menulis",
         )
 
+    def test_profile_if_user_is_logged_in(self):
+        
         client = Client()
         client.login(username="username",password="password")
-        url = reverse('cv:profile')
+        url = reverse(CV_PROFILE)
         response = client.get(url)
         self.assertEquals(response.status_code , 200)
 
     def test_profile_if_user_is_not_logged_in_should_redirect_to_login(self):
         client = Client()
-        url = reverse('cv:profile')
+        url = reverse(CV_PROFILE)
         response = client.get(url)
         self.assertEquals(response.status_code, 302)
 
-    def test_profile_should_return_certain_attribute(self):
-        pass
+    def test_profile_on_render_should_return_CV(self):
+        client = Client()
+        client.login(username="username",password="password")
+        url = reverse(CV_PROFILE)
+        response = client.get(url)
+        self.assertEquals(response.context['cv'].profile, "profile")
 
     def test_profile_should_render_profile_template(self):
         client = Client()
-        created_user = Users.objects.create(
-            username="username",
-            password="password",
-            npm=1,
-            prodi_id=1,
-            role_id=1
-        )
-        created_cv = CV.objects.create(
-            user=created_user,
-            profile="profile",
-            posisi = "manager",
-            instansi = "PT. CBA",
-            lama_instansi = "1 tahun",
-            keterangan_posisi = "mengurus jalannya PT. CBA",
-            asal_sekolah = "SMA Negeri 429",
-            masa_waktu = "2010-2013",
-            keterangan_pendidikan = "Sarjana",
-            kontak = "08123456789",
-            jenis_kontak = "WA",
-            kemampuan = "menguasai bahasa inggris",
-            prestasi = "Juara 1 lomba menulis",
-        )
 
         client.login(username="username",password="password")
-        url = reverse('cv:profile')
+        url = reverse(CV_PROFILE)
         response = client.get(url)
         self.assertTemplateUsed(response, 'cv/profile.html')
-        pass
 
-    def test_edit_profile_should_render_edit_profile_template(self):
-        pass
 
-    def test_edit_profile_should_change_field(self):
-        pass
 
