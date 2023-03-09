ae2e943325e0affa8a55fbda709e5dd6479842b3
Muhammad Rizqi Rizaldi <muhammad.rizqi03@ui.ac.id>
2023-03-09 23:33:18 +0700

    [GREEN] implement authentication

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from jobseeker.backend import CustomBackend
 
 # Create your views here.
+def home(request):
+    user = request.user
+    return render(request, 'home.html', {'user' : user})
+
+def login_user(request):
+    custom_backend = CustomBackend()
+    if request.method == 'POST':
+        user = custom_backend.authenticate(request)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('/login/')
+            
+    return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    return redirect('/login/')
