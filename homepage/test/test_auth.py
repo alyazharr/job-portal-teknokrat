82edd486f906b903742381fd2f28925a3239067e
Muhammad Rizqi Rizaldi <muhammad.rizqi03@ui.ac.id>
2023-03-10 23:42:07 +0700

    [REFACTOR] change to use django default auth

--- homepage/test/test_auth.py
+++ homepage/test/test_auth.py
@@ -11,6 +11,9 @@ class AuthenticationTest(TestCase):
         global HOMEPAGE_LOGOUT
         HOMEPAGE_LOGOUT = "homepage:logout"
 
+        global HOMEPAGE_HOME
+        HOMEPAGE_HOME = "homepage:home"
+
         self.client = Client()
         self.username = "testuser"
         self.password = "testpass"
@@ -29,13 +32,15 @@ class AuthenticationTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'login.html')
 
+    def test_success_login(self):
         # User success log in
-        login = self.client.login(username=self.username,password=self.password)
-        self.assertTrue(login)
+        response = self.client.post(reverse(HOMEPAGE_LOGIN), {'username':self.username, 'password':self.password})
+        self.assertRedirects(response, reverse(HOMEPAGE_HOME))
 
+    def test_fail_login(self):
         # User fail log in
-        login = self.client.login(username=self.username,password="wrongpass")
-        self.assertFalse(login)
+        response = self.client.post(reverse(HOMEPAGE_LOGIN), {'username':self.username, 'password':"wrongpass"})
+        self.assertRedirects(response, reverse(HOMEPAGE_LOGIN))
 
     def test_logout_view(self):
         # Log in the user
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
-from jobseeker.backend import CustomBackend
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def home(request):
@@ -8,10 +7,11 @@ def home(request):
     return render(request, 'home.html', {'user' : user})
 
 def login_user(request):
-    custom_backend = CustomBackend()
     if request.method == 'POST':
-        user = custom_backend.authenticate(request)
-        if user:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
             login(request, user)
             return redirect('/')
         else:
