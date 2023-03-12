bb1f0a606da5af781105f61aba5ee725b56f18a3
Veloraine <bivanderr@gmail.com>
2023-03-12 12:11:15 +0700

    [CHORE] Fix Conflict

--- jobseeker/settings.py
+++ jobseeker/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "jobseeker",
-    "cv",,
+    "cv",
     "homepage",
 ]
 
--- jobseeker/urls.py
+++ jobseeker/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("cv.urls")),
-    path('', include('homepage.urls')),
+    path("", include('homepage.urls')),
 ]
