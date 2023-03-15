fe33e3585d5f5da4ec7688e193e24cf21390e99a
Muhammad Rizqi Rizaldi <muhammad.rizqi03@ui.ac.id>
2023-03-15 21:26:57 +0700

    [REFACTOR] remove unused comments and files

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
 def home(request):
     user = request.user
     return render(request, 'home.html', {'user' : user})
