2eab4654e2fbfa480bce5addb1372b14920105a3
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2023-03-22 17:55:01 +0700

    [REFACTOR] PBI-11-Dashboard-Proposal-Lowongan

--- dashboard_proposal_lowongan/tests/test_dashboard.py
+++ dashboard_proposal_lowongan/tests/test_dashboard.py
@@ -32,33 +32,36 @@ DATE_FORMAT = "%Y-%m-%d"
 global MSG_NO_ADMIN
 MSG_NO_ADMIN = 'Anda tidak memiliki akses ke laman ini. Silakan login sebagai Admin.'
 
-class DashboardProposalLowonganTestNoLogin(TestCase):
-    def setUp(self) -> None:
-        PASSWORD_PERUSAHAAN = "Perusahaan123"
-        USERNAME_PERUSAHAAN = "iamperusahaan"
-        NPM_PERUSAHAAN = 34567890
-        PRODI_ID_PERUSAHAAN = 84202
-        ROLE_ID_PERUSAHAAN=2
+class BaseTestCase(TestCase):
+    PASSWORD_PERUSAHAAN = "Perusahaan123"
+    USERNAME_PERUSAHAAN = "iamperusahaan"
+    NPM_PERUSAHAAN = 34567890
+    PRODI_ID_PERUSAHAAN = 84202
+    ROLE_ID_PERUSAHAAN = 2
 
-        self.perusahaan = Users.objects.create (
-            username=USERNAME_PERUSAHAAN,
-            password=PASSWORD_PERUSAHAAN,
-            npm=NPM_PERUSAHAAN,
-            prodi_id=PRODI_ID_PERUSAHAAN,
-            role_id=ROLE_ID_PERUSAHAAN
+    def setUp(self) -> None:
+        self.perusahaan = Users.objects.create(
+            username=self.USERNAME_PERUSAHAAN,
+            password=self.PASSWORD_PERUSAHAAN,
+            npm=self.NPM_PERUSAHAAN,
+            prodi_id=self.PRODI_ID_PERUSAHAAN,
+            role_id=self.ROLE_ID_PERUSAHAAN
         )
+        self.perusahaan.save()
 
         self.lowongan = Lowongan.objects.create(
-            users_id = self.perusahaan,
+            users_id=self.perusahaan,
             posisi="Data Scientist",
             gaji=8000000,
-            lama_pengalaman= 10,
-            deskripsi= "Ahli dalam bidang data dan ML",
+            lama_pengalaman=10,
+            deskripsi="Ahli dalam bidang data dan ML",
             requirements=json.dumps(["s1"]),
             buka_lowongan=datetime.today().strftime(DATE_FORMAT),
             batas_pengumpulan=(datetime.today() + timedelta(days=1)).strftime(DATE_FORMAT)
         )
-    
+        self.lowongan.save()
+
+class DashboardProposalLowonganTestNoLogin(BaseTestCase):  
     def test_dashboard_response(self):
         response = Client().get(DASHBOARD)
         self.assertEqual(response.status_code, 302)
@@ -74,55 +77,32 @@ class DashboardProposalLowonganTestNoLogin(TestCase):
     def test_detail_page(self):
         url = reverse(DETAIL, args=[self.lowongan.id])
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 302)    
+        self.assertEqual(response.status_code, 302)  
 
-class DashboardProposalLowonganTestNotAdmin(TestCase):
+class DashboardProposalLowonganTestNotAdmin(BaseTestCase):
+    PASSWORD = "NotAdmin123"
+    NAME = "Not Admin Dummy"
+    USERNAME = "iamnotadminofteknokrat"
+    NPM = 12345678
+    PRODI_ID = 84202
+    
     def setUp(self) -> None:
-        PASSWORD = "NotAdmin123"
-        NAME = "Not Admin Dummy"
-        USERNAME = "iamnotadminofteknokrat"
-        NPM = 12345678
-        PRODI_ID = 84202
+        super().setUp()
 
         self.client = Client()
         alumni_acc = Users.objects.create(
-            username=USERNAME,
-            password=PASSWORD,
-            name=NAME,
-            npm=NPM,
-            prodi_id=PRODI_ID,
+            username=self.USERNAME,
+            password=self.PASSWORD,
+            name=self.NAME,
+            npm=self.NPM,
+            prodi_id=self.PRODI_ID,
             role_id=2
         )
 
-        PASSWORD_PERUSAHAAN = "Perusahaan123"
-        USERNAME_PERUSAHAAN = "iamperusahaan"
-        NPM_PERUSAHAAN = 34567890
-        PRODI_ID_PERUSAHAAN = 84202
-        ROLE_ID_PERUSAHAAN=2
-
-        self.perusahaan = Users.objects.create (
-            username=USERNAME_PERUSAHAAN,
-            password=PASSWORD_PERUSAHAAN,
-            npm=NPM_PERUSAHAAN,
-            prodi_id=PRODI_ID_PERUSAHAAN,
-            role_id=ROLE_ID_PERUSAHAAN
-        )
-
-        self.lowongan = Lowongan.objects.create(
-            users_id = self.perusahaan,
-            posisi=POSISI,
-            gaji=8000000,
-            lama_pengalaman= 10,
-            deskripsi= DESKRIPSI,
-            requirements=json.dumps(["s1"]),
-            buka_lowongan=datetime.today().strftime(DATE_FORMAT),
-            batas_pengumpulan=(datetime.today() + timedelta(days=1)).strftime(DATE_FORMAT)
-        )
-        
         self.alumni_acc = alumni_acc
-        login = self.client.login(username=USERNAME,password=PASSWORD)       
+        login = self.client.login(username=self.USERNAME,password=self.PASSWORD)       
         self.assertTrue(login)
-
+        
     def test_dashboard_response(self):
         response = self.client.get(DASHBOARD)
         self.assertEqual(response.status_code, 302)
@@ -156,55 +136,27 @@ class DashboardProposalLowonganTestNotAdmin(TestCase):
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]), 'Anda tidak memiliki akses ke laman ini. Silakan login sebagai Admin.')
 
-class DashboardProposalLowonganTestAdmin(TestCase):  
+class DashboardProposalLowonganTestAdmin(BaseTestCase):  
+    PASSWORD = "dummy-admin"
+    USERNAME = "admin-teknokrat"
+    NAME = "Admin Dummy"
+    NPM = 200767334
+    PRODI_ID = 84202
+
     def setUp(self) -> None:
-        PASSWORD = "dummy-admin"
-        USERNAME = "admin-teknokrat"
-        NAME = "Admin Dummy"
-        NPM = 200767334
-        PRODI_ID = 84202
+        super().setUp()
 
         self.client = Client()
         admin_acc = Users.objects.create(
-                username=USERNAME,
-                password=PASSWORD,
-                name=NAME,
-                npm=NPM,
-                prodi_id=PRODI_ID,
+                username=self.USERNAME,
+                password=self.PASSWORD,
+                name=self.NAME,
+                npm=self.NPM,
+                prodi_id=self.PRODI_ID,
                 role_id=4
             )
-        
-        PASSWORD_PERUSAHAAN = "Perusahaan123"
-        USERNAME_PERUSAHAAN = "iamperusahaan"
-        NAME_PERUSAHAAN = "PT PERUSAHAAN"
-        NPM_PERUSAHAAN = 34567890
-        PRODI_ID_PERUSAHAAN = 84202
-        ROLE_ID_PERUSAHAAN=2
 
-        self.perusahaan = Users.objects.create (
-            username=USERNAME_PERUSAHAAN,
-            password=PASSWORD_PERUSAHAAN,
-            name=NAME_PERUSAHAAN,
-            npm=NPM_PERUSAHAAN,
-            prodi_id=PRODI_ID_PERUSAHAAN,
-            role_id=ROLE_ID_PERUSAHAAN
-        )
-
-        lowongan = Lowongan.objects.create(
-            users_id = self.perusahaan,
-            posisi=POSISI,
-            gaji=8000000,
-            lama_pengalaman= 10,
-            deskripsi= DESKRIPSI,
-            requirements=json.dumps(["s1"]),
-            buka_lowongan=datetime.today().strftime(DATE_FORMAT),
-            batas_pengumpulan=(datetime.today() + timedelta(days=1)).strftime(DATE_FORMAT)
-        )
-
-        self.lowongan = lowongan
-        self.lowongan.save()
-
-        login = self.client.login(username=USERNAME,password=PASSWORD)
+        login = self.client.login(username=self.USERNAME,password=self.PASSWORD)
         self.admin_acc = admin_acc
         self.assertTrue(login)
     
