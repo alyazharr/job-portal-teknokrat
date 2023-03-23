7e3ea62e0c406c88bc8ef28820f5072f9bbfaaaf
Ikramullah <ikramullah@ui.ac.id>
2023-03-23 12:41:29 +0700

    Bugfix

--- jobseeker/settings.py
+++ jobseeker/settings.py
@@ -113,6 +113,7 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
